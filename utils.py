ca629440dd0f6e8c6723e6990f8db5a6a3b7f0a6
Zach <zmcgrath96@gmail.com>
2020-02-22 17:09:57 -0700

    added utils.py

---
+++ utils.py
@@ -0,0 +1,13 @@
+from get_tweets import get_tweets_by_hashtag
+
+def __has_n_tweets(hashtag, n=100):
+    '''
+    Determines if a hashtag has set number of tweets about it
+
+    Inputs:
+        hashtag:    string the hashtag in quesiton
+        n:          int number of tweets this hashtag should have. Default=100
+    Outputs:
+        True if the hashtag has at least n tweets in it False otherwise
+    '''
+    return True if len(get_tweets_by_hashtag(hashtag, n)) >= n else False
