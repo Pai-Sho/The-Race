3a24c6d5bb38c545706d45dc51fbab302b82438e
Pai-Sho <jackhessburg94@gmail.com>
2020-02-23 03:03:50 -0700

    Added form to game

--- flasktest.py
+++ flasktest.py
@@ -7,8 +7,8 @@ from flask import request, redirect
 from user import *
 
 class MyForm(FlaskForm):
-    start = StringField('#Start', [Length(min=1, max=25)], default='Bananas')
-    end = StringField('#End', [Length(min=1, max=25)], default='EpsteinDidntKillHimself')
+    start = StringField('#Start', [Length(min=1, max=25)])
+    end = StringField('#End', [Length(min=1, max=25)])
 
 
 
@@ -25,40 +25,13 @@ def home():
 
 @app.route('/game', methods=['POST'])
 def game():
+    form = MyForm(meta={'csrf': False})
     start = request.args.get('start')
     end = request.args.get('end')
-    #print(start, type(start))
     list1 = user_round(start)
-    #print('list1:', list1)
-    #a = user_round()
-    return render_template('game.html', start=start, list1=list1)
-
-'''
-@app.route('/', methods=('GET', 'POST'))
-def home():
-    form = MyForm(csrf_enabled=False)
-    print('HHHHHHEEEEEERRRRRRRRREEEEEEE')
-    if form.validate_on_submit():
-        start = form.start.data
-        end = form.end.data
-        print(start,end)
-        print('WTF')
-        return redirect(url_for('game'))
-    global count
-    count += 1
-
-    bot = Bot(4, 3, DEBUG=True)
-    path = bot.search('trump', 'hitler')
-    print(path)
-    return render_template('Home.html', form=form)
-
-@app.route('/gamestart', methods=('GET','POST'))
-def gamestart():
-    stuff = request.args()
-    print(stuff)
-    print('REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE')
-    return render_template('gamestart.html', stuff = stuff)
-'''
+    if form.validate_on_submit() and (form.start.data != start or form.end.data != end):
+        return redirect(url_for('game',start=form.start.data, end=form.end.data), code='307')
+    return render_template('game.html', start=start, list1=list1, form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
