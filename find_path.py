34d41111d8464d9e6888f287de39ddf491c847bd
Zach <zmcgrath96@gmail.com>
2020-02-22 23:11:05 -0700

    added function for getting dom element and removed prints

--- find_path.py
+++ find_path.py
@@ -1,8 +1,6 @@
 from typing import List, Dict, Optional
 
 from get_tweets import get_tweets_by_hashtag, get_tweets_by_hashtag_pair
-
-
 ##############################################################
 #                   Private Functions
 ##############################################################
@@ -16,11 +14,7 @@ def __get_related_tweet(hashtag1: str, hashtag2: str, num_tweets: int) -> Option
     Returns:
         list of strings of urls that relate the two hashtags
     '''
-    contains_both = []
-
     tweets = get_tweets_by_hashtag_pair(hashtag1, hashtag2, num_tweets)
-    print(hashtag1, hashtag2)
-    print([t['hashtags'] for t in tweets])
     for twt in tweets:
         if hashtag2 in twt['hashtags']:
             return twt['url']
@@ -44,7 +38,6 @@ def find_path(path, num_tweets=100):
         hashtag1 = path[i]
         hashtag2 = path[i+1]
         related_tweet = __get_related_tweet(hashtag1, hashtag2, num_tweets)
-        print("\n\nrelated:", related_tweet)
         tweet_path.append(related_tweet)
 
     return tweet_path
--- integration_test.py
+++ integration_test.py
@@ -4,14 +4,20 @@ from sort_hashtags import *
 from find_path import *
 from user import *
 
-START = 'furries'
-END = 'nazi'
+START = 'trump'
+END = 'putin'
 
 bot = Bot(4, 3, DEBUG=True)
 
 print('Bot run')
 path = bot.search(START, END)
 print(path)
+
+# url_path = find_path(path)
+# print('URL PATH [0]')
+# print(url_path[0])
+# maybe_dom = get_tweet_embedding(url_path[0])
+# print(maybe_dom)
 
 print('user run')
 temp = '{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n'
@@ -28,3 +34,4 @@ while True:
         print('oof')
     tags = user_round(next_tag)
     print(temp.format(*tags))
+
